263b25a5cb98ad5e494bb61702514ac0cb2375ec
Rafał <90453529+ZiumC@users.noreply.github.com>
2024-12-30 09:53:01 +0100

    [Task-7] created income draft

--- BudgetTracker/BudgetTracker/src/app/ui/pages/budget/budget.component.ts
+++ BudgetTracker/BudgetTracker/src/app/ui/pages/budget/budget.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SpinnerSize} from "../../components/shared/spinner/spinner.component";
 
 @Component({
   selector: 'app-budget',
   templateUrl: './budget.component.html',
   styleUrl: './budget.component.css'
 })
-export class BudgetComponent {
+export class BudgetComponent implements OnInit {
+  protected readonly SpinnerSize = SpinnerSize;
+  protected isLoaded: boolean = false;
+  protected selectedIncome: boolean = false;
 
+  ngOnInit(): void {
+    this.isLoaded = true;
+  }
 }
