ed02ece12ca5f5e1846468b2e3b82140852ec3be
Rafał <90453529+ZiumC@users.noreply.github.com>
2025-01-12 10:44:21 +0100

    [Task-24] implemented logic that disables next and prev buttons

--- BudgetTracker/BudgetTracker/src/app/ui/components/shared/pagination/pagination.component.ts
+++ BudgetTracker/BudgetTracker/src/app/ui/components/shared/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
@@ -6,19 +6,50 @@ import {Component, OnInit} from '@angular/core';
   styleUrl: './pagination.component.css'
 })
 export class PaginationComponent implements OnInit {
+  @Input() lastPage: number;
   protected page: number;
-  protected pageSize: number;
+  protected disablePrevious: boolean;
+  protected disableNext: boolean;
+
 
   ngOnInit(): void {
     this.page = 1;
-    this.pageSize = 12;
+
+    if (this.page == this.lastPage) {
+      this.disableNext = true;
+    } else {
+      this.disableNext = false;
+    }
+
+    this.disablePrevious = true;
+
+    if (!this.lastPage){
+      this.disableNext = true;
+      this.disablePrevious = true;
+    }
   }
 
   protected previous(): void {
     this.page--;
+
+    if (this.page <= 1) {
+      this.disablePrevious = true;
+    }
+
+    if (this.page < this.lastPage) {
+      this.disableNext = false;
+    }
   }
 
   protected next(): void {
     this.page++;
+
+    if (this.page > 1) {
+      this.disablePrevious = false;
+    }
+
+    if (this.page >= this.lastPage) {
+      this.disableNext = true;
+    }
   }
 }
