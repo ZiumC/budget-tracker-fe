128a1273d9e8caf1e33ccdb8a9aa04e04b1b44ee
Rafał <90453529+ZiumC@users.noreply.github.com>
2025-05-04 14:17:52 +0200

    [Task-67] integrated page size, page and order components by with be

--- BudgetTracker/BudgetTracker/src/app/ui/components/page/category/category/category.component.ts
+++ BudgetTracker/BudgetTracker/src/app/ui/components/page/category/category/category.component.ts
@@ -14,131 +14,148 @@ import {HttpResponse} from "@angular/common/http";
 import {generateErrorModel} from "../../../../../util/http.util";
 import {RequestParams} from "../../../../../models/requestParams";
 import {PageDto} from "../../../../../models/dto/page.model.dto";
+import {SortPayment} from "../../../../../util/sort.utils";
 
 @Component({
-    selector: 'app-category',
-    templateUrl: './category.component.html',
-    styleUrl: './category.component.css'
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrl: './category.component.css'
 })
 export class CategoryComponent implements OnInit, OnDestroy {
-    @ViewChild('errorModal') errorModal: any;
-    @Input() type: CategoryType;
-    @Input() displayName: string;
-    protected readonly formatString = formatString;
-    protected readonly DateUtil = DateUtil;
-    protected appConfig: AppConfig;
-    protected subscriptions: Subscription[];
-    protected categoryResponseModel: ResponseModel;
-    protected categoryRequestParams: RequestParams;
-    protected categoriesDto: GetCategoryDto[] | null;
-    protected selectedCategory: GetCategoryDto;
-    protected categoriesLoader: boolean;
-    protected categoriesTotalPages: number;
-    public innerWidth: any;
+  @ViewChild('errorModal') errorModal: any;
+  @Input() type: CategoryType;
+  @Input() displayName: string;
+  protected readonly formatString = formatString;
+  protected readonly DateUtil = DateUtil;
+  protected appConfig: AppConfig;
+  protected subscriptions: Subscription[];
+  protected categoryResponseModel: ResponseModel;
+  protected categoryRequestParams: RequestParams;
+  protected categoriesDto: GetCategoryDto[] | null;
+  protected selectedCategory: GetCategoryDto;
+  protected categoriesLoader: boolean;
+  protected categoriesTotalPages: number;
+  public innerWidth: any;
 
-    constructor(
-        private httpService: HttpService,
-        private configService: ConfigService) {
+  constructor(
+    private httpService: HttpService,
+    private configService: ConfigService) {
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(): void {
+    this.innerWidth = window.innerWidth;
+  }
+
+  ngOnInit(): void {
+    const appCfg = this.configService.getAppConfig();
+    if (appCfg) {
+      this.appConfig = appCfg;
+    } else {
+      throw Error("Config not provided");
     }
 
-    @HostListener('window:resize', ['$event'])
-    onResize(): void {
-        this.innerWidth = window.innerWidth;
+    if (!this.type) {
+      throw Error("Category type is required");
     }
 
-    ngOnInit(): void {
-        const appCfg = this.configService.getAppConfig();
-        if (appCfg) {
-            this.appConfig = appCfg;
-        } else {
-            throw Error("Config not provided");
+    this.categoryResponseModel = new ResponseModel();
+    this.categoryRequestParams = new RequestParams({
+      page: this.appConfig.request.pagination.defaultPage,
+      pageSize: this.appConfig.request.pagination.defaultPageSizeOptions[0],
+    })
+    this.subscriptions = [];
+
+    this.defaultOrderParams();
+    this.getCategories();
+  }
+
+  ngOnDestroy(): void {
+    SubscriptionUtils.unsubscribeAll(this.subscriptions);
+  }
+
+  onOrderEvent(orderOptions: OrderOptions): void {
+    if (orderOptions.orderType.applyForApi) {
+      this.categoryRequestParams.orderBy = orderOptions.orderType.value;
+      if (orderOptions.orderType.displayDirections) {
+        this.categoryRequestParams.order = orderOptions.orderDirection!.value;
+      } else {
+        this.categoryRequestParams.order = null;
+      }
+      this.onRefreshCategories();
+    }
+  }
+
+  onPageSizeEvent(pageSize: number): void {
+    this.categoryRequestParams.page = this.appConfig.request.pagination.defaultPage;
+    this.categoryRequestParams.pageSize = pageSize;
+    this.onRefreshCategories();
+  }
+
+  onPageEvent(page: number): void {
+    this.categoryRequestParams.page = page;
+    this.onRefreshCategories();
+  }
+
+  protected onRefreshCategories(): void {
+    this.markCategoriesAsLoaded(false);
+    this.getCategories();
+  }
+
+  private getCategories(): void {
+    this.subscriptions.push(
+      this.httpService.getCategories(
+        this.categoryRequestParams,
+        this.type).subscribe({
+        next: (response: HttpResponse<GetCategoryDto[]>): void => {
+          this.categoriesDto = response.body;
+          this.categoryResponseModel.statusCode = response.status;
+        },
+        error: (err): void => {
+          const response = generateErrorModel(err);
+          this.categoryResponseModel = response;
+          if (response.statusCode != 404) {
+            this.errorModal.open(response);
+          }
+          this.markCategoriesAsLoaded(true);
+        },
+        complete: (): void => {
+          this.getCategoriesTotalPages();
+          this.markCategoriesAsLoaded(true);
         }
+      })
+    )
+  }
 
-        if (!this.type) {
-            throw Error("Category type is required");
-        }
-
-        this.categoryResponseModel = new ResponseModel();
-        this.categoryRequestParams = new RequestParams({
-            page: this.appConfig.request.pagination.defaultPage,
-            pageSize: this.appConfig.request.pagination.defaultPageSizeOptions[0],
-        })
-        this.subscriptions = [];
-
-        this.defaultOrderParams();
-        this.getCategories();
-    }
-
-    ngOnDestroy(): void {
-        SubscriptionUtils.unsubscribeAll(this.subscriptions);
-    }
-
-    onOrderEvent(orderOptions: OrderOptions): void {
-
-    }
-
-    onPageSizeEvent(pageSize: number): void {
-
-    }
-
-    onPageEvent(page: number): void {
-
-    }
-
-    private getCategories(): void {
-        this.subscriptions.push(
-            this.httpService.getCategories(
-                this.categoryRequestParams,
-                this.type).subscribe({
-                next: (response: HttpResponse<GetCategoryDto[]>): void => {
-                    this.categoriesDto = response.body;
-                    this.categoryResponseModel.statusCode = response.status;
-                },
-                error: (err): void => {
-                    const response = generateErrorModel(err);
-                    this.categoryResponseModel = response;
-                    if (response.statusCode != 404) {
-                        this.errorModal.open(response);
-                    }
-                    this.markCategoriesAsLoaded(true);
-                },
-                complete: (): void => {
-                    this.getCategoriesTotalPages();
-                    this.markCategoriesAsLoaded(true);
-                }
-            })
-        )
-    }
-
-    private markCategoriesAsLoaded(isLoaded: boolean): void {
-        if (isLoaded) {
-            new TimerUtils(this.appConfig.animation.duration.default).start()
-                .subscribe(finished => {
-                    if (finished) {
-                        this.categoriesLoader = isLoaded;
-                    }
-                });
-        } else {
+  private markCategoriesAsLoaded(isLoaded: boolean): void {
+    if (isLoaded) {
+      new TimerUtils(this.appConfig.animation.duration.default).start()
+        .subscribe(finished => {
+          if (finished) {
             this.categoriesLoader = isLoaded;
+          }
+        });
+    } else {
+      this.categoriesLoader = isLoaded;
+    }
+  }
+
+  private getCategoriesTotalPages(): void {
+    this.subscriptions.push(
+      this.httpService.getCategoryPages(
+        this.categoryRequestParams,
+        this.type).subscribe({
+        next: (response: HttpResponse<PageDto>): void => {
+          this.categoriesTotalPages = response.body!.pages;
         }
-    }
+      })
+    )
+  }
 
-    private getCategoriesTotalPages(): void {
-        this.subscriptions.push(
-            this.httpService.getCategoryPages(
-                this.categoryRequestParams,
-                this.type).subscribe({
-                next: (response: HttpResponse<PageDto>): void => {
-                    this.categoriesTotalPages = response.body!.pages;
-                }
-            })
-        )
-    }
-
-    private defaultOrderParams(): void {
-        this.categoryRequestParams.orderBy =
-            this.appConfig.request.order.paymentCategoryTypes[0].value;
-        this.categoryRequestParams.order =
-            this.appConfig.request.order.orderDirections[0].value;
-    }
+  private defaultOrderParams(): void {
+    this.categoryRequestParams.orderBy =
+      this.appConfig.request.order.paymentCategoryTypes[0].value;
+    this.categoryRequestParams.order =
+      this.appConfig.request.order.orderDirections[0].value;
+  }
 }
