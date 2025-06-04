4f472c096c9b05d25d31a0e120c3bdb745b0223c
Rafał <90453529+ZiumC@users.noreply.github.com>
2025-06-04 19:27:18 +0200

    [Task-78] implemented modal steps and forms for that

--- BudgetTracker/BudgetTracker/src/app/ui/components/modals/copy-payment-modal/copy-payment-modal.component.ts
+++ BudgetTracker/BudgetTracker/src/app/ui/components/modals/copy-payment-modal/copy-payment-modal.component.ts
@@ -1,18 +1,30 @@
-import {Component, ViewChild} from '@angular/core';
-import {ModalOptions} from "../../../../util/modal.utils";
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {ModalOptions, ModalSize, ModalUtils} from "../../../../util/modal.utils";
 import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import {HttpService} from "../../../../services/http/http.service";
 import {ConfigService} from "../../../../services/config/config.service";
+import {formatString} from "../../../../util/string.utils";
+import {FormBuilder, FormGroup, NgModel, Validators} from "@angular/forms";
+import {DatePicker} from "../../../../models/datepicker.model";
+import {DatePickerUtil, DateUtil, isInvalidDate} from "../../../../util/date.util";
+import {AppConfig} from "../../../../models/config/config";
+import {FormConfig} from "../../../../models/config/form.model.config";
 
 @Component({
   selector: 'app-copy-payment-modal',
   templateUrl: './copy-payment-modal.component.html',
   styleUrl: './copy-payment-modal.component.css'
 })
-export class CopyPaymentModalComponent {
+export class CopyPaymentModalComponent implements OnInit, OnDestroy {
   @ViewChild('copyPaymentModal') copyModal: any;
+  protected readonly formatString = formatString;
+  protected readonly ModalUtils = ModalUtils;
+  protected currentStep: number = 1;
+  protected fromDatePicker: DatePicker = new DatePicker();
+  protected toDatePicker: DatePicker = new DatePicker();
+  protected appConfig: AppConfig;
+  protected formConfig: FormConfig;
 
-  step = 1;
 
   constructor(
     private modalService: NgbModal,
@@ -20,24 +32,61 @@ export class CopyPaymentModalComponent {
     private configService: ConfigService) {
   }
 
-  open() {
-    this.modalService.open(this.copyModal, ModalOptions.default());
+
+  ngOnDestroy(): void {
   }
 
-
-  protected onSelectData(data: any):void {
-    this.goNext();
+  ngOnInit(): void {
+    const appCfg = this.configService.getAppConfig();
+    if (appCfg) {
+      this.appConfig = appCfg;
+      this.formConfig = appCfg.form;
+    } else {
+      throw Error("Config not provided")
+    }
   }
+
+  protected searchBudgets(): void {
+    let fromDate = DatePickerUtil.convertToDate(this.fromDatePicker);
+    let toDate = DatePickerUtil.convertToDate(this.toDatePicker);
+  }
+
+  protected onDatesChanged(fromDateInput: NgModel, toDateInput: NgModel): void {
+    const isInvalidFromDate = isInvalidDate(this.fromDatePicker);
+    const isInvalidToDate = isInvalidDate(this.toDatePicker);
+
+    if (isInvalidFromDate) {
+      fromDateInput.control.setErrors({ngbDate: true});
+    } else if (isInvalidToDate) {
+      toDateInput.control.setErrors({ngbDate: true});
+    } else {
+      const fromDate = DatePickerUtil.convertToDate(this.fromDatePicker);
+      const toDate = DatePickerUtil.convertToDate(this.toDatePicker);
+
+      if (toDate <= fromDate) {
+        fromDateInput.control.setErrors({invalidRange: true});
+        toDateInput.control.setErrors({invalidRange: true});
+      } else {
+        fromDateInput.control.setErrors(null);
+        toDateInput.control.setErrors(null);
+      }
+    }
+  }
+
+  open(): void {
+    this.modalService.open(this.copyModal, ModalOptions.default(ModalSize.BIG));
+  }
+
 
   protected goNext(): void {
-    this.step = this.step + 1;
+    this.currentStep = this.currentStep + 1;
   }
 
   protected goBack(): void {
-    this.step = this.step - 1;
+    this.currentStep = this.currentStep - 1;
   }
 
-  private goToStep(stepNumber: number) {
-    this.step = stepNumber;
+  protected save(): void {
+
   }
 }
