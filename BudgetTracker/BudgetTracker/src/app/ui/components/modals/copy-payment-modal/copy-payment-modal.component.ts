aa73671bd7fcdb833ce0e966a5fd9bc2c95d2fbf
Rafał <90453529+ZiumC@users.noreply.github.com>
2025-06-04 21:16:51 +0200

    [Task-78] implemented auto fill with dates from +1 month to +7 months on each modal open

--- BudgetTracker/BudgetTracker/src/app/ui/components/modals/copy-payment-modal/copy-payment-modal.component.ts
+++ BudgetTracker/BudgetTracker/src/app/ui/components/modals/copy-payment-modal/copy-payment-modal.component.ts
@@ -4,9 +4,9 @@ import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import {HttpService} from "../../../../services/http/http.service";
 import {ConfigService} from "../../../../services/config/config.service";
 import {formatString} from "../../../../util/string.utils";
-import {FormBuilder, FormGroup, NgModel, Validators} from "@angular/forms";
+import {NgModel} from "@angular/forms";
 import {DatePicker} from "../../../../models/datepicker.model";
-import {DatePickerUtil, DateUtil, isInvalidDate} from "../../../../util/date.util";
+import {DatePickerUtil, DateUtil} from "../../../../util/date.util";
 import {AppConfig} from "../../../../models/config/config";
 import {FormConfig} from "../../../../models/config/form.model.config";
 
@@ -20,8 +20,8 @@ export class CopyPaymentModalComponent implements OnInit, OnDestroy {
   protected readonly formatString = formatString;
   protected readonly ModalUtils = ModalUtils;
   protected currentStep: number = 1;
-  protected fromDatePicker: DatePicker = new DatePicker();
-  protected toDatePicker: DatePicker = new DatePicker();
+  protected fromDatePicker: DatePicker;
+  protected toDatePicker: DatePicker;
   protected appConfig: AppConfig;
   protected formConfig: FormConfig;
 
@@ -42,41 +42,50 @@ export class CopyPaymentModalComponent implements OnInit, OnDestroy {
       this.appConfig = appCfg;
       this.formConfig = appCfg.form;
     } else {
-      throw Error("Config not provided")
+      throw Error("Config not provided");
     }
+
+    const fromDate = DateUtil.setMonthsToDate(DateUtil.firstDayOfCurrentMonth(), 1);
+    const toDate = DateUtil.setMonthsToDate(DateUtil.lastDayOfCurrentMonth(), 7);
+
+    this.fromDatePicker = DatePickerUtil.convertToDatePicker(fromDate);
+    this.toDatePicker = DatePickerUtil.convertToDatePicker(toDate);
+
+    this.searchBudgets();
   }
 
   protected searchBudgets(): void {
-    let fromDate = DatePickerUtil.convertToDate(this.fromDatePicker);
-    let toDate = DatePickerUtil.convertToDate(this.toDatePicker);
+    let fromDate: Date = DatePickerUtil.convertToDate(this.fromDatePicker);
+    let toDate: Date = DatePickerUtil.convertToDate(this.toDatePicker);
+
+    //TO DO...
   }
 
   protected onDatesChanged(fromDateInput: NgModel, toDateInput: NgModel): void {
-    const isInvalidFromDate = isInvalidDate(this.fromDatePicker);
-    const isInvalidToDate = isInvalidDate(this.toDatePicker);
-
-    if (isInvalidFromDate) {
-      fromDateInput.control.setErrors({ngbDate: true});
-    } else if (isInvalidToDate) {
-      toDateInput.control.setErrors({ngbDate: true});
-    } else {
-      const fromDate = DatePickerUtil.convertToDate(this.fromDatePicker);
-      const toDate = DatePickerUtil.convertToDate(this.toDatePicker);
-
-      if (toDate <= fromDate) {
-        fromDateInput.control.setErrors({invalidRange: true});
-        toDateInput.control.setErrors({invalidRange: true});
-      } else {
-        fromDateInput.control.setErrors(null);
-        toDateInput.control.setErrors(null);
-      }
-    }
+    // const isInvalidFromDate = isInvalidDate(this.fromDatePicker);
+    // const isInvalidToDate = isInvalidDate(this.toDatePicker);
+    //
+    // if (isInvalidFromDate) {
+    //   fromDateInput.control.setErrors({ngbDate: true});
+    // } else if (isInvalidToDate) {
+    //   toDateInput.control.setErrors({ngbDate: true});
+    // } else {
+    //   const fromDate = DatePickerUtil.convertToDate(this.fromDatePicker);
+    //   const toDate = DatePickerUtil.convertToDate(this.toDatePicker);
+    //
+    //   if (toDate <= fromDate) {
+    //     fromDateInput.control.setErrors({invalidRange: true});
+    //     toDateInput.control.setErrors({invalidRange: true});
+    //   } else {
+    //     fromDateInput.control.setErrors(null);
+    //     toDateInput.control.setErrors(null);
+    //   }
+    // }
   }
 
   open(): void {
     this.modalService.open(this.copyModal, ModalOptions.default(ModalSize.BIG));
   }
-
 
   protected goNext(): void {
     this.currentStep = this.currentStep + 1;
