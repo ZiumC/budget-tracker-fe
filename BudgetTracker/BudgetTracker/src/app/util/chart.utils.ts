50044672d4910144f7e14de89d8945cb4e53164b
Rafał <90453529+ZiumC@users.noreply.github.com>
2025-10-19 13:24:43 +0200

    [Task-101] refactored code

--- BudgetTracker/BudgetTracker/src/app/util/chart.utils.ts
+++ BudgetTracker/BudgetTracker/src/app/util/chart.utils.ts
@@ -1,14 +1,12 @@
 import {
-  GetIncomeStatsDto, GetPlannedPaymentStatsDto, GetRegularPaymentStatsDto,
-  IncomeCategoryDto, PlannedPaymentCategoryDto, RegularPaymentCategoryDto
+  GetIncomeStatsDto,
+  IncomeCategoryDto,
 } from "../models/statistics.model";
 import {add, subtract} from "./number.util";
 import BigNumber from "bignumber.js";
-import {HorizontalBarDataResult, ChartDataResult, LineChartResult} from "../models/charts.model";
+import { ChartDataResult} from "../models/charts.model";
 import {
   GetBudgetGeneralCategoryDto,
-  GetBudgetStatisticsSummaryDto,
-  GetBudgetSummaryDto
 } from "../models/dto/budget.model.dto";
 
 export function formatPercent(input: string): string {
@@ -47,38 +45,6 @@ export function transformToIncomeDto(data: GetIncomeStatsDto | null): IncomeCate
   return result;
 }
 
-export function transformToRegularDto(data: GetRegularPaymentStatsDto | null): RegularPaymentCategoryDto[] {
-  let result: RegularPaymentCategoryDto[] = [];
-  if (data) {
-    Object.entries(data).forEach(([key, value]): void => {
-      if ('PriceSum' in value && 'RefundSum' in value) {
-        result.push({
-          name: key,
-          PriceSum: value.PriceSum,
-          RefundSum: value.RefundSum
-        } as RegularPaymentCategoryDto);
-      }
-    });
-  }
-  return result;
-}
-
-export function transformToPlannedDto(data: GetPlannedPaymentStatsDto | null): PlannedPaymentCategoryDto[] {
-  let result: PlannedPaymentCategoryDto[] = [];
-  if (data) {
-    Object.entries(data).forEach(([key, value]): void => {
-      if ('PriceSum' in value && 'EstimatedSum' in value) {
-        result.push({
-          name: key,
-          PriceSum: value.PriceSum,
-          EstimatedSum: value.EstimatedSum
-        } as PlannedPaymentCategoryDto);
-      }
-    });
-  }
-  return result;
-}
-
 export function incomeToPieChartData(incomeDetails: IncomeCategoryDto[]): ChartDataResult[] {
   let result: ChartDataResult[] = [];
   let totalSavings = new BigNumber(0);
@@ -101,69 +67,6 @@ export function incomeToPieChartData(incomeDetails: IncomeCategoryDto[]): ChartD
   return result;
 }
 
-export function regularPaymentToPieChartData(regularDetails: RegularPaymentCategoryDto[]): ChartDataResult[] {
-  let result: ChartDataResult[] = [];
-  let totalRefund = new BigNumber(0);
-
-  for (let regularDetail of regularDetails) {
-    totalRefund = add(new BigNumber(totalRefund), new BigNumber(regularDetail.RefundSum));
-    result.push({
-      name: regularDetail.name,
-      value: subtract(new BigNumber(regularDetail.PriceSum), new BigNumber(regularDetail.RefundSum)).toNumber()
-    });
-  }
-
-  if (totalRefund.toNumber() > 0) {
-    result.push({
-      name: 'Refund',
-      value: totalRefund.toNumber()
-    } as ChartDataResult)
-  }
-
-  return result;
-}
-
-export function plannedPaymentToPieChartData(plannedDetails: PlannedPaymentCategoryDto[]): ChartDataResult[] {
-  let result: ChartDataResult[] = [];
-
-  for (let plannedDetail of plannedDetails) {
-    result.push({
-      name: plannedDetail.name,
-      value: new BigNumber(plannedDetail.EstimatedSum).toNumber()
-    });
-  }
-
-  return result;
-}
-
-export function budgetUsageToHorizontalChartData(data: GetBudgetStatisticsSummaryDto | null): HorizontalBarDataResult[] {
-  let result: HorizontalBarDataResult[] = [];
-
-  if (data) {
-    const moneySpend = add(new BigNumber(data.regularPayment.paid), new BigNumber(data.plannedPayment.paid));
-    const onlyIncomes = subtract(new BigNumber(data.income.wage), new BigNumber(data.income.savings));
-    const incomeLeft = add(subtract(onlyIncomes, moneySpend), new BigNumber(data.income.budgetSurplus));
-    result.push({
-      name: 'Budget',
-      series: [
-        {
-          name: "Money spend",
-          value: moneySpend.toNumber(),
-        },
-        {
-          name: "Money left",
-          value: incomeLeft.toNumber() < 0 ? 0 : incomeLeft.toNumber(),
-        },
-        {
-          name: "Overuse",
-          value: incomeLeft.toNumber() < 0 ? incomeLeft.abs().toNumber() : 0,
-        }
-      ]
-    } as HorizontalBarDataResult)
-  }
-
-  return result;
-}
 
 export function generalCategoriesToPieChartGrid(data: GetBudgetGeneralCategoryDto | null): ChartDataResult[] {
   let result: ChartDataResult[] = [];
--- BudgetTracker/BudgetTracker/src/app/util/chart/budget/budget-payment.chart.util.ts
+++ BudgetTracker/BudgetTracker/src/app/util/chart/budget/budget-payment.chart.util.ts
@@ -1,6 +1,13 @@
-import {GetBudgetSummaryDto} from "../../../models/dto/budget.model.dto";
-import {ChartDataResult, LineChartResult} from "../../../models/charts.model";
+import {GetBudgetStatisticsSummaryDto, GetBudgetSummaryDto} from "../../../models/dto/budget.model.dto";
+import {ChartDataResult, HorizontalBarDataResult, LineChartResult} from "../../../models/charts.model";
 import BigNumber from "bignumber.js";
+import {add, subtract} from "../../number.util";
+import {
+  GetPlannedPaymentStatsDto, GetRegularPaymentStatsDto,
+  IncomeCategoryDto,
+  PlannedPaymentCategoryDto,
+  RegularPaymentCategoryDto
+} from "../../../models/statistics.model";
 
 enum SeriesType {
   PRICE = 'Price',
@@ -16,8 +23,8 @@ export enum PaymentChartType {
   PLANNED_PAYMENTS = 'savings and surplus'
 }
 
-export class BudgetPaymentSummary {
-  private static createSeries(budgets: GetBudgetSummaryDto[], seriesType: SeriesType): ChartDataResult[] {
+class ChartLineUtil {
+  static createSeries(budgets: GetBudgetSummaryDto[], seriesType: SeriesType): ChartDataResult[] {
     let seriesResult: ChartDataResult[] = [];
 
     for (let budget of budgets) {
@@ -52,11 +59,76 @@ export class BudgetPaymentSummary {
 
     return seriesResult;
   }
+}
+
+export class BudgetPaymentSummary {
+  static toPieChartData(payments: GetPlannedPaymentStatsDto | GetRegularPaymentStatsDto | null): ChartDataResult[] {
+    let result: ChartDataResult[] = [];
+    let totalRefund = new BigNumber(0);
+
+    if (payments) {
+      Object.entries(payments).forEach(([key, value]): void => {
+        //regular payment
+        if ('PriceSum' in value && 'RefundSum' in value) {
+          totalRefund = add(new BigNumber(totalRefund), new BigNumber(value.RefundSum));
+          result.push({
+            name: key,
+            value: subtract(new BigNumber(value.PriceSum), new BigNumber(value.RefundSum)).toNumber()
+          } as ChartDataResult);
+
+          //planned payment
+        } else if ('PriceSum' in value && 'EstimatedSum' in value) {
+          result.push({
+            name: key,
+            value: new BigNumber(value.EstimatedSum).toNumber()
+          } as ChartDataResult);
+        } else {
+          throw Error("Unsupported type of payment " + payments);
+        }
+      });
+
+      if (totalRefund.toNumber() > 0) {
+        result.push({
+          name: 'Refund',
+          value: totalRefund.toNumber()
+        } as ChartDataResult)
+      }
+    }
+    return result;
+  }
+
+  static toHorizontalChart(data: GetBudgetStatisticsSummaryDto | null): HorizontalBarDataResult[] {
+    let result: HorizontalBarDataResult[] = [];
+
+    if (data) {
+      const moneySpend = add(new BigNumber(data.regularPayment.paid), new BigNumber(data.plannedPayment.paid));
+      const onlyIncomes = subtract(new BigNumber(data.income.wage), new BigNumber(data.income.savings));
+      const incomeLeft = add(subtract(onlyIncomes, moneySpend), new BigNumber(data.income.budgetSurplus));
+      result.push({
+        name: 'Budget',
+        series: [
+          {
+            name: "Money spend",
+            value: moneySpend.toNumber(),
+          },
+          {
+            name: "Money left",
+            value: incomeLeft.toNumber() < 0 ? 0 : incomeLeft.toNumber(),
+          },
+          {
+            name: "Overuse",
+            value: incomeLeft.toNumber() < 0 ? incomeLeft.abs().toNumber() : 0,
+          }
+        ]
+      } as HorizontalBarDataResult);
+    }
+    return result;
+  }
 
   static toLineChart(budgetsSummary: GetBudgetSummaryDto[] | null, chartType: PaymentChartType): LineChartResult[] {
     let result: LineChartResult[] = [];
 
-    if (budgetsSummary){
+    if (budgetsSummary) {
       let seriesType1: SeriesType;
       let seriesType2: SeriesType;
       let seriesType3: SeriesType;
@@ -69,17 +141,17 @@ export class BudgetPaymentSummary {
           seriesType1 = SeriesType.ESTIMATED;
           seriesType2 = SeriesType.REAL;
           seriesType3 = SeriesType.PLANNED_PAID;
-          seriesResult1 = BudgetPaymentSummary.createSeries(budgetsSummary, seriesType1);
-          seriesResult2 = BudgetPaymentSummary.createSeries(budgetsSummary, seriesType2);
-          seriesResult3 = BudgetPaymentSummary.createSeries(budgetsSummary, seriesType3);
+          seriesResult1 = ChartLineUtil.createSeries(budgetsSummary, seriesType1);
+          seriesResult2 = ChartLineUtil.createSeries(budgetsSummary, seriesType2);
+          seriesResult3 = ChartLineUtil.createSeries(budgetsSummary, seriesType3);
           break;
         case PaymentChartType.REGULAR_PAYMENTS:
           seriesType1 = SeriesType.PRICE;
           seriesType2 = SeriesType.REFUND;
           seriesType3 = SeriesType.REGULAR_PAID;
-          seriesResult1 = BudgetPaymentSummary.createSeries(budgetsSummary, seriesType1);
-          seriesResult2 = BudgetPaymentSummary.createSeries(budgetsSummary, seriesType2);
-          seriesResult3 = BudgetPaymentSummary.createSeries(budgetsSummary, seriesType3);
+          seriesResult1 = ChartLineUtil.createSeries(budgetsSummary, seriesType1);
+          seriesResult2 = ChartLineUtil.createSeries(budgetsSummary, seriesType2);
+          seriesResult3 = ChartLineUtil.createSeries(budgetsSummary, seriesType3);
           break;
         default:
           throw Error(chartType + ' is unsupported chart');
