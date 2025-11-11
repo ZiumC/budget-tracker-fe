eb469b1814b6dc1805af7287b051c0e97cff29a5
Rafał <90453529+ZiumC@users.noreply.github.com>
2025-11-11 11:20:18 +0100

    [Task-103] implemented feature that resets user password

--- BudgetTracker/BudgetTracker/src/app/util/tostr.util.ts
+++ BudgetTracker/BudgetTracker/src/app/util/tostr.util.ts
@@ -4,7 +4,7 @@ import {getErrorResponse} from "./http.util";
 export class ToastUtil {
   static handleErrorResponse(toastService: ToastrService, err: any): void {
     const statusCode: number = err.status;
-    if (statusCode >= 200 && statusCode <= 299){
+    if (statusCode >= 200 && statusCode <= 299) {
       throw new Error("Should be error instead of success");
     }
     const responseModel = getErrorResponse(err);
@@ -12,5 +12,21 @@ export class ToastUtil {
     let message: string = responseModel.error.message;
 
     toastService.error(message, title);
+  }
+
+  static accountRegistered(toastService: ToastrService, email: string): void {
+    toastService.success(`Verification code has been send to email ${email}`, "Account has been registered!");
+  }
+
+  static passwordResetRequested(toastService: ToastrService, email: string): void {
+    toastService.success(`Temporary password has been sent to email ${email}`, "Requested for password change!");
+  }
+
+  static emailConfirmed(toastService: ToastrService): void {
+    toastService.success("Email has been confirmed!");
+  }
+
+  static passwordSuccessfullySet(toastService: ToastrService): void {
+    toastService.success("Password has been changed!");
   }
 }
